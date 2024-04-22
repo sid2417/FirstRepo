2cf9ec5bc3403698e45fb825439f0b5c076ed270
sid2417 <siddhartha.java9398@gmail.com>
2024-04-22 16:11:47 +0530

    i committed all files at a time

---
+++ git-steps.sh
@@ -0,0 +1,18 @@
+** Git Steps**
+First create any repo in your local
+second in your VStudio open that folder
+third in your main file like md you write your code
+fourth open gitbash in your local, and go to your repos folder 
+
+now follow below steps::
+
+git clone <repos name>
+
+git add <repose name>
+
+git commit -m "Write some message"
+Note::First time it is asking Email and Username
+
+git push -u main origin
+Note::It is asking Token to commit
+
